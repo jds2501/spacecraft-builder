8cf8ab41ccbe0c33bd75e767d2614c96664dfd28
Jason Smith <jasondsmith2501@gmail.com>
2025-02-08 18:25:03 -0800

    Added error handling to form

--- src/ItemForm.jsx
+++ src/ItemForm.jsx
@@ -2,8 +2,24 @@ import React from 'react';
 import { useFormik } from 'formik';
 import "./ItemForm.css";
 
+const AGREE_TO_TERMS = "itemFormAgreeToTerms";
+const NAME = "itemFormName";
+const QUANTITY = "itemFormQuantity";
+const PURPOSE = "itemFormPurpose";
+const REQUIRED_STRING_FIELDS = [NAME, QUANTITY, PURPOSE];
+
 const validate = values => {
     const errors = {};
+
+    for (let index = 0; index < REQUIRED_STRING_FIELDS.length; index++) {
+        if (values[REQUIRED_STRING_FIELDS[index]] === "") {
+            errors[REQUIRED_STRING_FIELDS[index]] = true;
+        }
+    }
+
+    if (!values[AGREE_TO_TERMS]) {
+        errors[AGREE_TO_TERMS] = true;
+    }
 
     return errors;
 };
@@ -11,31 +27,36 @@ const validate = values => {
 const ItemForm = () => {
     const formik = useFormik({
         initialValues: {
-            itemFormName: "",
-            itemFormQuantity: "",
-            itemFormPurpose: "",
-            itemFormAgreeToTerms: ""
+            [NAME]: "",
+            [QUANTITY]: "",
+            [PURPOSE]: "",
+            [AGREE_TO_TERMS]: false
         },
         validate,
+        validateOnChange: false,
+        validateOnBlur: false,
         onSubmit: values => {
-            alert(JSON.stringify(values, null, 2));
+            console.log(JSON.stringify(values, null, 2));
         },
     });
+
+    console.log(formik.errors);
+
     return (
         <>
             <h2>Add an Item to the Inventory</h2>
             <form className="ItemForm" onSubmit={formik.handleSubmit}>
                 <input
-                    className="ItemFormInput"
-                    name="itemFormName"
+                    className={formik.errors[NAME] ? "ItemFormInput-error" : "ItemFormInput"}
+                    name={NAME}
                     type="text"
                     placeholder="Name"
                     onChange={formik.handleChange}
-                    value={formik.values.itemFormName}
+                    value={formik.values[NAME]}
                 />
 
                 <input
-                    className="ItemFormInput"
+                    className={formik.errors.itemFormQuantity ? "ItemFormInput-error" : "ItemFormInput"}
                     name="itemFormQuantity"
                     type="text"
                     placeholder="Quantity"
@@ -44,23 +65,23 @@ const ItemForm = () => {
                 />
 
                 <textarea
-                    className="ItemFormInput"
+                    className={formik.errors.itemFormPurpose ? "ItemFormInput-error" : "ItemFormInput"}
                     name="itemFormPurpose"
                     placeholder="Purpose"
                     rows={"4"}
+                    onBlur={formik.handleBlur}
                     onChange={formik.handleChange}
                     value={formik.values.itemFormPurpose}
                 />
 
-                <div className="ItemFormInput ItemFormCheckbox">
+                <div className={formik.errors[AGREE_TO_TERMS] ? "ItemFormInput-error ItemFormCheckbox" : "ItemFormInput ItemFormCheckbox"}>
                     <input
-                        name="itemFormAgreeToTerms"
+                        name={AGREE_TO_TERMS}
                         type="checkbox"
-                        placeholder="Quantity"
                         onChange={formik.handleChange}
-                        value={formik.values.itemFormQuantity}
+                        value={formik.values.itemFormAgreeToTerms}
                     />
-                    <label htmlFor="itemFormAgreeToTerms">Agree to Terms</label>
+                    <label htmlFor={AGREE_TO_TERMS}>Agree to Terms</label>
                 </div>
 
                 <div className="ItemFormSubmit">
